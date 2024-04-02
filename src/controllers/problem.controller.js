36686c949a526b3d201c56521e981f6efffa8391
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-04-02 13:58:56 +0530

    Fix: Migrated From Numeric Status Codes To HTTP Status Codes Library And Corrected The Error Message

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,25 +1,39 @@
+const { StatusCodes } = require("http-status-codes");
+
 function pingProblemController(req, res) {
-  return res.status(501).json({ message: "Problem Controller Is Up" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 function addProblem(req, res) {
-  return res.status(501).json({ message: "Problem Controller Is Up" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 function getProblem(req, res) {
-  return res.status(501).json({ message: "Problem Controller Is Up" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 function getProblems(req, res) {
-  return res.status(501).json({ message: "Problem Controller Is Up" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 function deleteProblem(req, res) {
-  return res.status(501).json({ message: "Problem Controller Is Up" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 function updateProblem(req, res) {
-  return res.status(501).json({ message: "Problem Controller Is Up" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 module.exports = {
