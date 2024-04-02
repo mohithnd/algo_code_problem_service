4b89e44c0e26ed92f6c371b749052c66d35c9674
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-04-02 15:41:58 +0530

    Feature: Added Error handler Middleware With Basic Error Classes

---
+++ src/errors/interbalServer.error.js
@@ -0,0 +1,15 @@
+const BaseError = require("./base.error");
+const { StatusCodes } = require("http-status-codes");
+
+class InternalServer extends BaseError {
+  constructor(details) {
+    super(
+      "InternalServerError",
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      "Something Went Wrong",
+      details
+    );
+  }
+}
+
+module.exports = InternalServer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
+const errorHandler = require("./utils/errorHandler");
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use("/api", apiRouter);
 app.get("/ping", (req, res) => {
   return res.json({ message: "Problem Service Is Alive" });
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server Is Listening On: ${PORT}`);
